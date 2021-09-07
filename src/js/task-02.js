e019a7fec65fa1d6b9a11cdb2e7390e7a60b79ef
Roman Vozniuk <muzzlestore@gmail.com>
2021-09-07 23:19:09 +0300

    second task done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+const ingredientsListEl = document.querySelector('#ingredients');
+
+const makeIngredientsList = ingredients => {
+   return ingredients.map(ingredient => {
+      const ingredientEl = document.createElement('li');
+      ingredientEl.textContent = ingredient;
+      return ingredientEl;
+  })
+}
+const ingredientsList = makeIngredientsList(ingredients);
+
+ingredientsListEl.append(...ingredientsList);
